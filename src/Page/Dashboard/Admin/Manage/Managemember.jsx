eb028ffb8f116085f67df64bd6a1b522e29015b0
mubashirbappi <mubashirbappi@gmail.com>
2025-01-17 03:01:46 +0600

    manage member section added

--- src/Page/Dashboard/Admin/Manage/Managemember.jsx
+++ src/Page/Dashboard/Admin/Manage/Managemember.jsx
@@ -1,9 +1,47 @@
 import React from 'react';
+import useAxiosSecure from './../../../../Hooks/useAxiosSecure';
+import { useQuery } from '@tanstack/react-query';
 
 const Managemember = () => {
+    const axiosSecure = useAxiosSecure();
+    const {data:users=[],refetch}=useQuery({
+        queryKey:'users',
+        queryFn:async()=>{
+            const res = await axiosSecure.get('/users')
+            const alluser = res.data
+          const members =  alluser.filter(user=>user.role==='member')
+         return members
+        }
+    })
     return (
         <div>
-            <h1>this is  manage member </h1>
+           <div className="overflow-x-auto">
+  <table className="table table-zebra">
+    {/* head */}
+    <thead>
+      <tr>
+        <th></th>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Action</th>
+      </tr>
+    </thead>
+    <tbody>
+        {
+            users.map((user,idx)=><tr>
+                <th>{idx+1}</th>
+                <td>{user.userName}</td>
+                <td>{user.userEmail}</td>
+                <td><button className="btn">Remove</button></td>
+              </tr>)
+        }
+     
+      
+     
+     
+    </tbody>
+  </table>
+</div>
             
         </div>
     );
