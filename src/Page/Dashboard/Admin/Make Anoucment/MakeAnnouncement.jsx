16bdf085df22d6f953c72617d368b74f8f250d80
mubashirbappi <mubashirbappi@gmail.com>
2025-01-17 03:34:34 +0600

    make announcement section added

--- src/Page/Dashboard/Admin/Make Anoucment/MakeAnnouncement.jsx
+++ src/Page/Dashboard/Admin/Make Anoucment/MakeAnnouncement.jsx
@@ -1,9 +1,50 @@
 import React from 'react';
+import useAxiosPublic from './../../../../Hooks/useAxiosPublic';
 
 const MakeAnnouncement = () => {
+      const axiospublic = useAxiosPublic()
+     const handleannounce = (e)=>{
+        e.preventDefault()
+        const form = e.target 
+        const title = form.title.value
+        const announce = form.announcement.value
+      
+        const announcement = {
+             title:title,
+             notice:announce
+        }
+        axiospublic.post('/notice',announcement)
+        .then(res=>{
+            console.log(res.data)
+            if(res.data.insertedId){
+                alert('notice sent')
+            }
+        })
+     }
     return (
         <div>
-            <h1>this is make annoucement </h1>
+          <div className='w-7/12 mx-auto'>
+            <form onSubmit={handleannounce}>
+            <div className="form-control">
+            <label className="label">
+            <span className="label-text">Title</span>
+          </label>
+            <input name='title' type="text" placeholder="Type here" className="input input-bordered w-full " />
+            </div>
+            <div className="form-control">
+            <label className="label">
+            <span className="label-text">Description</span>
+          </label>
+          <textarea name='announcement' className="textarea textarea-bordered" rows={5} placeholder="Type announcement.."></textarea>
+            </div>
+           <div className='form-control mt-4'>
+            <input type="submit" className='btn' value="Submit" />
+
+           </div>
+
+            </form>
+        
+          </div>
             
         </div>
     );
