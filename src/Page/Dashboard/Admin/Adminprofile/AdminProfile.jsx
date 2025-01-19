9ef719a4e00853485162a68fcacc1b6835a6daa8
mubashirbappi <mubashirbappi@gmail.com>
2025-01-19 12:02:48 +0600

    admin profile complate

--- src/Page/Dashboard/Admin/Adminprofile/AdminProfile.jsx
+++ src/Page/Dashboard/Admin/Adminprofile/AdminProfile.jsx
@@ -1,9 +1,69 @@
 import React from 'react';
+import useAuth from '../../../../Hooks/useAuth';
+import { useQuery } from '@tanstack/react-query';
+import useAxiosPublic from '../../../../Hooks/useAxiosPublic';
+import useAxiosSecure from '../../../../Hooks/useAxiosSecure';
 
 const AdminProfile = () => {
+    const {user}=useAuth()
+    const axiosSecure = useAxiosSecure()
+    const {data:report=[]}=useQuery({
+      queryKey:'report',
+      queryFn:async()=>{
+        const res = await axiosSecure.get('/adminreport')
+        return res.data
+      }
+    })
+  const  availableApartmets = report.totalApartments - report.totalAgreement;
+  const PercentageofAgreement = (report.totalAgreement/report.totalApartments)*100;
+  const PercentageofavailableApartment = (availableApartmets/report.totalApartments)*100;
     return (
         <div>
-            <h1>this is admin profile</h1>
+           <section>
+            <div className='border flex'>
+           <img className='w-60' src={user.photoURL}alt="" />
+           <div>
+            <h1>{user.displayName}</h1>
+            <h5>{user.email}</h5>
+
+           </div>
+           
+
+            </div>
+            <div>
+            <div className="stats stats-vertical lg:stats-horizontal shadow">
+  <div className="stat">
+    <div className="stat-title">Users</div>
+    <div className="stat-value">{report.totalUsers}</div>
+  
+  </div>
+
+  <div className="stat">
+    <div className="stat-title">Member</div>
+    <div className="stat-value">{report.totalMembers}</div>
+  
+  </div>
+
+  <div className="stat">
+    <div className="stat-title">Total Apartments</div>
+    <div className="stat-value">{report.totalApartments}</div>
+   
+  </div>
+  <div className="stat">
+    <div className="stat-title">Total Agreement</div>
+    <div className="stat-value">{report.  totalAgreement}</div>
+    <div className="stat-desc">↘︎ {report.totalApartments } ({PercentageofAgreement}%)</div>
+   
+  </div>
+  <div className="stat">
+    <div className="stat-title">Available Apartmets</div>
+    <div className="stat-value">{availableApartmets}</div>
+    <div className="stat-desc">↘︎ {report.totalApartments } ({PercentageofavailableApartment}%)</div>
+   
+  </div>
+</div>
+            </div>
+           </section>
             
         </div>
     );
--- src/Page/Dashboard/member/Pay History/PayHistory.jsx
+++ src/Page/Dashboard/member/Pay History/PayHistory.jsx
@@ -1,11 +1,53 @@
-import React from 'react';
+import React from "react";
+import useAuth from "../../../../Hooks/useAuth";
+import useAxiosSecure from "../../../../Hooks/useAxiosSecure";
+import moment from "moment";
+import { useQuery } from "@tanstack/react-query";
 
 const PayHistory = () => {
-    return (
-        <div>
-            <h1>this is pay history</h1>
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const { data: payhistory = [] } = useQuery({
+    queryKey: ["payhistory", user.email],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/payments/${user?.email}`);
+      return res.data;
+    },
+  });
+  return (
+    <div>
+      <h1>this is pay history</h1>
+
+      <section>
+        <div className="overflow-x-auto">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th></th>
+                <th>Date.</th>
+                <th>Transaction Id</th>
+                <th>Rent Pay</th>
+              </tr>
+            </thead>
+            <tbody>
+              {payhistory.map((history, idx) => (
+                <tr className="bg-base-200">
+                  <th>{idx + 1}</th>
+                  <td>
+                    {moment(history.date).subtract(10, "days").calendar()}
+                  </td>
+                  <td>{history.transactionId}</td>
+                  <td>{history.price}</td>
+                </tr>
+              ))}
+              {/* row 1 */}
+            </tbody>
+          </table>
         </div>
-    );
+      </section>
+    </div>
+  );
 };
 
 export default PayHistory;
