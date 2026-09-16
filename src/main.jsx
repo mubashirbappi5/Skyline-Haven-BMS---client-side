903cd1386ac2aa58a9e09ec81c5d0cb903e6e7c7
mubashirbappi5 <mubashirbappi@gmail.com>
2026-09-16 14:15:00 +0600

    Configure Google OAuth Provider Client ID

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 import MainRoute from './route/MainRoute.jsx'
 import { RouterProvider } from 'react-router-dom'
 import AuthProvider from './Provider/AuthProvider/AuthProvider.jsx'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import {
   QueryClient,
   QueryClientProvider,
@@ -13,13 +14,14 @@ import {
 
 const queryClient = new QueryClient();
 
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-     <QueryClientProvider client={queryClient}>
-   <AuthProvider>
-   <RouterProvider router={MainRoute} />
-   </AuthProvider>
-   </QueryClientProvider>
+    <GoogleOAuthProvider clientId="209772118200-j9mrj2fbn0pdij2gqs0l5vts3gsumu04.apps.googleusercontent.com">
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <RouterProvider router={MainRoute} />
+        </AuthProvider>
+      </QueryClientProvider>
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
