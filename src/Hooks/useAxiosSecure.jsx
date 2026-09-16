74bbff634f5f013390a94d471766a9d46a6b47c1
mubashirbappi5 <mubashirbappi@gmail.com>
2026-09-16 14:48:43 +0600

    Fix local backend URLs for auth

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import useAuth from "./useAuth";
 const axiosSecure = axios.create({
-  baseURL: "https://skyline-server-nine.vercel.app",
+  baseURL: "http://localhost:7000",
 });
 const useAxiosSecure = () => {
   const navigate = useNavigate();
