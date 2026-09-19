e71d36258edc787fab41da57473090f09926d1f7
mubashirbappi5 <mubashirbappi@gmail.com>
2026-09-19 15:02:04 +0600

    fix: case sensitive import for Coupons component

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -3,7 +3,7 @@ import Banner from './Components/Banner';
 import BannerImg from '../../Shared/BannerImg';
 import OurStats from './Components/OurStats';
 import About from './Components/About';
-import Coupons from './Components/coupons';
+import Coupons from './Components/Coupons';
 import Location from './Components/Location';
 import { Authcontext } from '../../Provider/AuthProvider/AuthProvider';
 import WeProvide from './Components/WeProvide';
